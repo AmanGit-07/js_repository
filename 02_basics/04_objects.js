76ece69bc3d2f31ede223070542f8498bc3093e4
AmanGit-07 <118670874+AmanGit-07@users.noreply.github.com>
2024-06-24 19:25:52 +0000

    OBJECT destructuring and json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -55,5 +55,110 @@ console.log(Object.keys(tinderUser));       //[ 'id', 'name', 'isLoggedIn' ]
 console.log(Object.values(tinderUser));     //[ '123abc', 'Sammy', false ]
 console.log(Object.entries(tinderUser));    //[ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));    //will tell whether this property is available or not
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));    //will tell whether this property is available or not true or false
 
+// +++++++++++++++++++OBJECT DESTRUCTURING++++++++++++++++++++++
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+};
+
+// console.log(course.courseInstructor);
+
+// const {courseInstructor} = course;       //for ouptput 1
+const {courseInstructor: instructor} = course;      //for output 2
+
+// console.log(courseInstructor);           //of expression 1
+console.log(instructor);                    //of expression 2
+
+
+// OBJECT DESTRUCTRING INSIDE A FUNCTION PARAMETER IN REACT
+// const navbar=({companyName})=>{ //here we are destructring the companyName 
+
+// };
+// navbar(company="Name");     //here we are passing the company name key of an object 
+
+
+// API comes in this format called json which is actually an object type
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+// url of the API: 'https://randomuser.me/api/'
+// A sample API example
+// {
+//   "results": [
+//     {
+//       "gender": "female",
+//       "name": {
+//         "title": "Miss",
+//         "first": "Jennie",
+//         "last": "Nichols"
+//       },
+//       "location": {
+//         "street": {
+//           "number": 8929,
+//           "name": "Valwood Pkwy",
+//         },
+//         "city": "Billings",
+//         "state": "Michigan",
+//         "country": "United States",
+//         "postcode": "63104",
+//         "coordinates": {
+//           "latitude": "-69.8246",
+//           "longitude": "134.8719"
+//         },
+//         "timezone": {
+//           "offset": "+9:30",
+//           "description": "Adelaide, Darwin"
+//         }
+//       },
+//       "email": "jennie.nichols@example.com",
+//       "login": {
+//         "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+//         "username": "yellowpeacock117",
+//         "password": "addison",
+//         "salt": "sld1yGtd",
+//         "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+//         "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+//         "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+//       },
+//       "dob": {
+//         "date": "1992-03-08T15:13:16.688Z",
+//         "age": 30
+//       },
+//       "registered": {
+//         "date": "2007-07-09T05:51:59.390Z",
+//         "age": 14
+//       },
+//       "phone": "(272) 790-0888",
+//       "cell": "(489) 330-2385",
+//       "id": {
+//         "name": "SSN",
+//         "value": "405-88-3636"
+//       },
+//       "picture": {
+//         "large": "https://randomuser.me/api/portraits/men/75.jpg",
+//         "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+//         "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+//       },
+//       "nat": "US"
+//     }
+//   ],
+//   "info": {
+//     "seed": "56d27f4a53bd5441",
+//     "results": 1,
+//     "page": 1,
+//     "version": "1.4"
+//   }
+// }
