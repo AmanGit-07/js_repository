16631a3937f64ad5071ed78f0a23e4e469f1f849
AmanGit-07 <118670874+AmanGit-07@users.noreply.github.com>
2026-08-06 17:06:55 +0000

    changes

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,31 +1,55 @@
-const marvel_heros = ["thor", "Ironman", "spiderman"];
-const dc_heros = ["superman", "flash", "batman"];
+// const marvel_heros = ["thor", "Ironman", "spiderman"];
+// const dc_heros = ["superman", "flash", "batman"];
 
-// marvel_heros.push(dc_heros)  //will give array inside an array dc_heros will be pushed as an another element inside marvel_heroes
+// // marvel_heros.push(dc_heros)  //will give array inside an array dc_heros will be pushed as an another element inside marvel_heroes
 
-// console.log(marvel_heros);              //[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
-// console.log(marvel_heros[3][1]);        //flash <- output
+// // console.log(marvel_heros);              //[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// // console.log(marvel_heros[3][1]);        //flash <- output
 
-const allHeros = marvel_heros.concat(dc_heros);  //will concat two array and give the output as a new array
-console.log(allHeros); //[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+// const allHeros = marvel_heros.concat(dc_heros);  //will concat two array and give the output as a new array
+// console.log(allHeros); //[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
 
 
-const all_new_heros = [...marvel_heros, ...dc_heros];
-// here we are using spread operators to access two arrays and as a name suggest it returns the elements of the array as in spreaded form that is only the elements so it combines all together to make a new array
+// const all_new_heros = [...marvel_heros, ...dc_heros];
+// // here we are using spread operators to access two arrays and as a name suggest it returns the elements of the array as in spreaded form that is only the elements so it combines all together to make a new array
 
-console.log(all_new_heros); //[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+// console.log(all_new_heros); //[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
 
-const real_another_array = another_array.flat(Infinity);     //returns a one d array at the end
-console.log(real_another_array);
+// const real_another_array = another_array.flat(Infinity);     //returns a one d array at the end
+// console.log(real_another_array);
 
-console.log(Array.isArray("Hitesh"));       //checks whether content inside method is isArray or not returns false or true
-console.log(Array.from("Hitesh"));      //converts string to array -> [ 'H', 'i', 't', 'e', 's', 'h' ]
-console.log(Array.from({name: "hitesh"})) // interesting    will give empty array
+// console.log(Array.isArray("Hitesh"));       //checks whether content inside method is isArray or not returns false or true
+// console.log(Array.from("Hitesh"));      //converts string to array -> [ 'H', 'i', 't', 'e', 's', 'h' ]
+// console.log(Array.from({name: "hitesh"})) // interesting    will give empty array
 
-let score1 = 100
-let score2 = 200
-let score3 = 300
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
 
-console.log(Array.of(score1, score2, score3));  //[ 100, 200, 300 ] gives an array of these elements
+// console.log(Array.of(score1, score2, score3));  //[ 100, 200, 300 ] gives an array of these elements
+
+
+
+const arr = ["aman", "bhatia", "rahul"];
+console.log(arr);
+let newArr = [];
+newArr = newArr.concat(arr);
+console.log(newArr);
+console.log(Array.isArray(newArr));
+console.log(Array.from(arr));
+let score1 = 100;
+let str = "cheek";
+console.log(Array.of(score1, str));
+
+
+const obj = {
+    firstName: "Aman",
+    lastName: "Bhatia",
+    age: 25,
+}
+console.log(Array.from(obj));   //it'll give an empty array and if you want an array of keys only
+console.log(Object.keys(obj));
+console.log(Object.values(obj));    //an array of values only
+console.log(Object.entries(obj));    //an array of all the [key:value] pair of an object
